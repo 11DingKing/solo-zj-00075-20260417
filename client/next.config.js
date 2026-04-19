1d869e1b3d80ba5b5392a7f9e6d91a412ce78a64
ding <ding@dingdeMac-mini-2805.local>
2026-04-19 11:19:53 +0800

    本 repo 题目 9 的第二轮

--- client/next.config.js
+++ client/next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   env: {
     SITE_NAME: process.env.SITE_NAME,
     API_URL: process.env.API_URL
+  },
+  publicRuntimeConfig: {
+    SITE_NAME: process.env.SITE_NAME,
+    API_URL: process.env.API_URL
   }
 }
